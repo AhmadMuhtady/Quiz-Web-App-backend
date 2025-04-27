9fd094ed2ac4269eb4ebf1e9846522484bfe0555
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 13:35:28 +0300

    added validation for required fields in signup.php

--- src/APIS/users/signup.php
+++ src/APIS/users/signup.php
@@ -13,3 +13,8 @@ $last_name = $_Post['last_name'] ?? null;
 $email = $_Post['email'] ?? null;
 $password_hash = $_Post['password_hash'] ?? null;
 
+if (empty($first_name) || empty($last_name) || empty($email) || empty($password_hash)) {
+    echo json_encode(["error" => "All fields are required."]);
+    exit;
+}
+$check_sql =
