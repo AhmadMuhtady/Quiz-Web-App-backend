d03091c9a77d56df5ad0a32803afb9e96a310648
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 20:42:41 +0300

    added quiz_id handling and validation

--- src/APIS/questions/get_all_questions.php
+++ src/APIS/questions/get_all_questions.php
@@ -6,3 +6,10 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     exit;
 }
 include "../../../confiq/connect.php";
+
+$quiz_id = $_POST['quiz_id'] ?? null;
+
+if (empty($question_id)) {
+    echo json_encode(["error" => "question_id is required."]);
+    exit;
+}
