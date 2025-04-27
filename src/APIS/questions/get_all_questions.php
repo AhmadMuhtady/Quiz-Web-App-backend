0bf75d49ffc4b733ce6ef62441ee82c11a542626
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 20:48:53 +0300

    fixed variable name

--- src/APIS/questions/get_all_questions.php
+++ src/APIS/questions/get_all_questions.php
@@ -9,7 +9,17 @@ include "../../../confiq/connect.php";
 
 $quiz_id = $_POST['quiz_id'] ?? null;
 
-if (empty($question_id)) {
+if (empty($quiz_id)) {
     echo json_encode(["error" => "question_id is required."]);
     exit;
 }
+
+$sql = 'SELECT * FROM questions WHERE quiz_id = ?';
+$stmt = mysqli_prepare($connection, $sql);
+
+if ($stmt) {
+    mysqli_stmt_bind_param($stmt, "i", $quiz_id); // bind quiz_id as integer
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    $questions = [];
