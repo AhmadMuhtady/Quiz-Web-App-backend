4e611fe176e7054a50a816a191714afee12226b3
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 13:03:10 +0300

    corrected error handling in database connection script

--- confiq/connect.php
+++ confiq/connect.php
@@ -8,6 +8,8 @@ $charset = 'utf8mb4';
 $connection = mysqli_connect($host, $user, $pass, $db);
 
 if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error()); 
+}else {
+    echo "Connected successfully!";
 }
 
