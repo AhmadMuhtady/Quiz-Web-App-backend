340d20df40ca26db5838eeb32f5bb28c292cf6a7
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 18:51:42 +0300

    added quiz_id validation

--- src/APIS/questions/create_question.php
+++ src/APIS/questions/create_question.php
@@ -14,3 +14,15 @@ if (empty($quiz_id) || empty($question_text)) {
     echo json_encode(["error" => "quiz_id and question_text are required."]);
     exit;
 }
+$check_quiz_sql = "SELECT quiz_id FROM quizzes WHERE quiz_id = ?";
+$check_quiz_stmt = mysqli_prepare($connection, $check_quiz_sql);
+
+if ($check_quiz_stmt) {
+    mysqli_stmt_bind_param($check_quiz_stmt, "i", $quiz_id);
+    mysqli_stmt_execute($check_quiz_stmt);
+    mysqli_stmt_store_result($check_quiz_stmt);
+
+    if (mysqli_stmt_num_rows($check_quiz_stmt) === 0) {
+        echo json_encode(["error" => "Quiz not found. Invalid quiz_id."]);
+        exit;
+    }
