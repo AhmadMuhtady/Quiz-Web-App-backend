73f63e4258b7d51ec12cc55c043f7586d50bad64
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 17:59:47 +0300

    removed POST request restriction in get_all_quizez.php

--- src/APIS/quizes/get_all_quizez.php
+++ src/APIS/quizes/get_all_quizez.php
@@ -1,10 +1,5 @@
 <?php
 header('Content-Type: application/json');
-
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    echo json_encode(["error" => "Only POST requests are allowed."]);
-    exit;
-}
 include "./../../../confiq/connect.php";
 
 $sql - 'SELECT * FROM quizzes';
