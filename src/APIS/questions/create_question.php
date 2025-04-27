724580fda1901167b2e01a5fb9025c9dd6c19da8
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 18:43:57 +0300

    added validation for quiz_id and question_text

--- src/APIS/questions/create_question.php
+++ src/APIS/questions/create_question.php
@@ -10,3 +10,7 @@ include "../../../confiq/connect.php";
 $quizzes = $_POST["quiz_id"] ?? null;
 $question_text = $_POST["question_text"] ?? null;
 
+if (empty($quiz_id) || empty($question_text)) {
+    echo json_encode(["error" => "quiz_id and question_text are required."]);
+    exit;
+}
