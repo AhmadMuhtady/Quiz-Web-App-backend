ff857522c19d6de6968132ce7921b04a03d0821f
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 12:49:27 +0300

    corrected error message formatting in database connection

--- confiq/connect.php
+++ confiq/connect.php
@@ -1,13 +1,13 @@
 <?php
 $host = 'localhost';
 $db = 'db_web_app';
-$user = 'root';
+$user = 'root@localhost';
 $pass = '';
 $charset = 'utf8mb4';
 
 $connection = mysqli_connect($host, $user, $pass, $db);
 
 if (!$connection) {
-    die("Connection failed" . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 
