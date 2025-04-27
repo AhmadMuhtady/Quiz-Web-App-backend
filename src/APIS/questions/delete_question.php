a39189b4eef8cb0889b245738e81c14b4dab0e47
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 20:02:02 +0300

    added  handling question deletion requests

---
+++ src/APIS/questions/delete_question.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo json_encode(["error" => "Only POST requests are allowed."]);
+    exit;
+}
+include "../../../confiq/connect.php";
+
+$question_id = $_POST['question_id'] ?? null;
