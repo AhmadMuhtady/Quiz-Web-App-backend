fbaa5b9602c63383be4997e08352ead4d56a4740
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 16:09:16 +0300

    added POST request

--- src/APIS/quizes/edit_quiz.php
+++ src/APIS/quizes/edit_quiz.php
@@ -6,3 +6,15 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     exit;
 }
 include "./../../../confiq/connect.php";
+
+$quiz_id = $_POST['quiz_id'] ?? null;
+$title = $_POST['title'] ?? null;
+$description = $_POST['description'] ?? null;
+
+if (empty($quiz_id) || empty($title) || empty($description)) {
+    echo json_encode(["error" => "Missing required fields."]);
+    exit;
+}
+
+$sql = "UPDATE quizzes SET title = ?, description = ? WHERE quiz_id = ?";
+$stmt = mysqli_prepare($connection, $sql);
