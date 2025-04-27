993c0a878addd14ce07591d70116449bcf029b36
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 16:37:14 +0300

    added delete_quiz.php

---
+++ src/APIS/quizes/delete_quiz.php
@@ -0,0 +1,7 @@
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo json_encode(["error" => "Only POST requests are allowed."]);
+    exit;
+}
+include "./../../../confiq/connect.php";
