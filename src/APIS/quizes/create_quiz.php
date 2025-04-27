e70f8de60672e1c1f2354704f525b15477098839
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 15:31:17 +0300

    added input handling for quiz creation fields in create_quiz.php

--- src/APIS/quizes/create_quiz.php
+++ src/APIS/quizes/create_quiz.php
@@ -6,3 +6,12 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     exit;
 }
 include "./../../../confiq/connect.php";
+
+$title = $_POST['title'] ?? null;
+$discription = $_POST['discription'] ?? null;
+$created_by = $_POST['created_by'] ?? null;
+
+if (empty($title) || empty($discription) || empty($created_by)) {   
+    echo json_encode(["error" => "Missing required fields"]);
+    exit;
+    
