8eb54550d20d91444e42b560f76677d6a7f939b2
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 12:31:43 +0300

    added database connection script for the quiz app

---
+++ confiq/connect.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$db = 'db_web_app';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$connection = mysqli_connect($host, $user, $pass, $db);
+
+if (!$connection) {
+    die("Connection failed" . mysqli_connect_error());
+}
+
