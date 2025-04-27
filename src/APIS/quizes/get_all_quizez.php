4d35718027447e885d72e6fa8dfec34e7b36a213
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 17:50:23 +0300

    corrected SQL query assignment operator in get_all_quizez.php

---
+++ src/APIS/quizes/get_all_quizez.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo json_encode(["error" => "Only POST requests are allowed."]);
+    exit;
+}
+include "./../../../confiq/connect.php";
+
+$sql - 'SELECT * FROM quizzes';
+$result = mysqli_query($connection, $sql);
+
+$quizzes = [];
