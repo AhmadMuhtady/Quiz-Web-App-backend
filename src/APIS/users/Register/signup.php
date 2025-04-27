40c551b90dcf36638058773e0ff90b62d0664391
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 13:27:36 +0300

    added missing variable assignments for user registration in signup.php

--- src/APIS/users/Register/signup.php
+++ src/APIS/users/Register/signup.php
@@ -9,3 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 include '../../../../confiq/connect.php';
 
 $first_name = $_Post['first_name'] ?? null;
+$last_name = $_Post['last_name'] ?? null;
+$email = $_Post['email'] ?? null;
+$password_hash = $_Post['password_hash'] ?? null;
+
