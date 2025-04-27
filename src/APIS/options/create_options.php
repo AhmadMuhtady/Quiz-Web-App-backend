0af2339553c56f43a7e81bca7c9cbec3e466c52f
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-27 21:33:33 +0300

    added validation for question_id, option_text, and is_correct

--- src/APIS/options/create_options.php
+++ src/APIS/options/create_options.php
@@ -6,3 +6,12 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     exit;
 }
 include "../../../confiq/connect.php";
+
+$question_id = $_POST["question_id"] ?? null;
+$option_text = $_POST["option_text"] ?? null;
+$is_correct = $_POST["is_correct"] ?? null;
+
+if (empty($question_id) || empty($option_text)) || empty($is_correct)) {
+    echo json_encode(["error"=> "quiz_id, question_text and is_correct are required"]);
+    exit;
+}
